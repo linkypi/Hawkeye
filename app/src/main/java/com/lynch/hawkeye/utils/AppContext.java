a0511999ce67c2786670aa25119f561d2682e3cd
lynch <trouble.linky@gmail.com>
2016-11-28 00:22:37 +0800

    main

---
+++ app/src/main/java/com/lynch/hawkeye/utils/AppContext.java
@@ -0,0 +1,12 @@
+package com.lynch.hawkeye.utils;
+
+/**
+ * Created by JL on 2016/11/26/0026.
+ */
+
+public class AppContext {
+    public static boolean hasLogin = false;
+    public static String user_name= "";
+    public static String password ;
+
+}
